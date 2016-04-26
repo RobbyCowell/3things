db7ca5d9a02d709fe58d43ac680047ce60ca606d
Robby Cowell <robby.cowell@gmail.com>
2016-04-26 13:26:24 +0100

    Configured webpack for Sass and added test styles

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Router, Route, IndexRoute, hashHistory } from "react-router";
+import '../css/style.scss';
 
 import Layout from "./pages/Layout";
 import Tasks from "./pages/Tasks";
